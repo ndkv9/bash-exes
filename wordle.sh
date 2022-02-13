ecbad4b41a2f92755835b052f7613e79a10da8d2
Vu <vu.nguyen101@gmail.com>
2022-02-13 11:25:55 +0200

    add max attemps for wordle game

--- wordle.sh
+++ wordle.sh
@@ -1,11 +1,25 @@
-WORD=MONGO
+echo -e "Ok, I picked a word, try to guess\n"
 GO_ON=1
 TRIES=0
-
 while [ $GO_ON -eq 1 ]
 do
-  TRIES=(expr $TRIES + 1)
+  TRIES=$(expr $TRIES + 1)
   read -n 5 -p "What is your guess: " USER_GUESS
+  USER_GUESS=$(echo "$USER_GUESS" | awk '{print toupper($0)}')
+  STATE=""
+  for i in {0..4}
+  do
+    if [ "${WORD:i:1}" == "${USER_GUESS:i:1}" ]
+    then
+      STATE=$STATE"🟩"
+    elif [[ $WORD =~ "${USER_GUESS:i:1}" ]]
+    then
+      STATE=$STATE"🟨"
+    else
+      STATE=$STATE"⬛️"
+    fi
+  done
+  echo "  "$STATE
   if [ $USER_GUESS == $WORD ]
   then
     echo -e "You won!"
